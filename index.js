c86419042a4ad7b98808fc8baf5f8f0e2fdd27f8
Nico <nico@Nicos-MacBook-Pro.local>
2019-01-06 20:50:12 +0200

    create reducers

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import App from './src/app';
 
 AppRegistry.registerComponent('auth', () => App);
